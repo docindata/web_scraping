9dc098820f99fe1a65b8338810e7d336d8a1feeb
docindata <92650082+docindata@users.noreply.github.com>
2022-12-02 17:46:08 +0100

    2dec

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # web_scraping
+
+This is a personal project, starting from virtually no specific knowledge of webscraping and aiming to 
+know enough to read, parse and structure data from websites in order to analyze it and maybe then create a
+model.
+
+## Ideas
+
+1. Restaurant data: Scrape data about all Gothenburg's restaurants in the website the fork and analyze the data
+to uncover the most popular restaurants.
--- tutorial.py
+++ tutorial.py
@@ -1,41 +1,64 @@
+# pylint: disable=W0105:pointless-string-statement
 # %% [markdown]
 """
 Starting with a simple py.file
 """
 # %%
-import requests 
-import bs4 # type: ignore
-from bs4 import BeautifulSoup # type: ignore
+import requests
+import bs4  # type: ignore
+from bs4 import BeautifulSoup  # type: ignore
+
+## %% [markdown]
+"""
+Now lets get HTML from a website with Titanic
+"""
 # %%
-result_prem: requests.models.Response = requests.get( # pylint: disable=W3101
-    "https://www.google.com"
-)
-content_prem: str = result_prem.text
-soup_prem: BeautifulSoup = BeautifulSoup(content_prem, "lxml")
-# %%
-print(soup_prem.prettify()) # This method makes the HTML readable
-# %%
-# Finding tags in the HTML can be done with the find method
-print(soup_prem.find('a', class_ = 'gb1'))
-# The args are the tag name and the class name. More on tag names in Miro
-# %%
-# find_all repeats the find method for all the tags in the HTML
-print(soup_prem.find_all('a'))
-# %% [markdown]
-# Now lets get HTML from a website with Titanic
-# %%
-website: str = r'https://subslikescript.com/movie/Titanic-120338'
-result: requests.models.Response = requests.get(website) # pylint: disable=W3101
+website: str = r"https://subslikescript.com/movie/Titanic-120338"
+result: requests.models.Response = requests.get(website, timeout = 5)  # pylint: disable=W3101
 soup: BeautifulSoup = BeautifulSoup(result.text, "lxml")
-# finding elements with a recommended order
+"""
+finding elements with a recommended order
 # 1. ID
 # 2. Class name
 # 3. Tag name, CSS selector
 # 4. Xpath
-box: bs4.element.Tag = soup.find('article', class_='main-article') # Simply locating the section
+"""
+box: bs4.element.Tag = soup.find(
+    "article", class_="main-article"
+)  # Simply locating the section
 # %%
-def get_text(tag: bs4.element.Tag) -> int:
+def get_text_from_website(url: str) -> str:
+    """_summary_
+
+    Args:
+        url (str): _description_
+
+    Returns:
+        str: _description_
     """
-    Gets the text from a tag
-    """
-    return tag.text
+    response: requests.models.Response = requests.get(url, timeout=5)
+    pretty_soup: BeautifulSoup = BeautifulSoup(response.text, "lxml").prettify()
+    return pretty_soup
+
+
+# %%
+soup_titanic: str = get_text_from_website(website)
+print(soup_titanic)
+print(type(soup_titanic))
+
+# %% [markdown]
+"""
+To get the text from the whole website is easy, and not that useful.
+Instead one shoud get the text from a specific section of the website. The tags and elements we
+mentioned before.
+
+An idea I want to explore is resaturant in the website: The fork.
+"""
+# %%
+spoton_url: str = r'https://www.thefork.se/restaurang/spoton-restaurang-r384077'
+result_spoton: requests.models.Response = requests.get(spoton_url, timeout=5)
+content_spoton: str = result_spoton.text
+soup_spoton: BeautifulSoup = BeautifulSoup(content_spoton, "lxml")
+menu_spoton: bs4.element.Tag = soup_spoton.find("dl", role="presentation")
+# %%
+print(menu_spoton)
